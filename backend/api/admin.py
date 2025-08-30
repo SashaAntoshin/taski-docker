d2aec241879a5fe0f91943c2ab0cb930fc600e19
SashaAntoshin <globeinwork@gmail.com>
2025-08-30 12:12:29 +0300

    Flake8 done.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,12 @@
+"""Admin configuration for API models."""
 from django.contrib import admin
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+    """Class task."""
+
     list_display = ('title', 'description', 'completed')
 
 
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,14 @@
+"""Serializers for API application."""
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """Serializer for Task model."""
+
     class Meta:
+        """Class meta."""
+
         model = Task
         fields = ('id', 'title', 'description', 'completed')
